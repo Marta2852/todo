4b1bf90e2d1760f5cf784c4275b7a45c54562266
ipa23.m.dzerve <ipa23.m.dzerve@pjvt.local>
2025-03-25 15:00:49 +0200

    edit update

--- resources/views/diaries/edit.blade.php
+++ resources/views/diaries/edit.blade.php
@@ -1,39 +1,42 @@
 <x-layout>
-  <x-slot:title>
-    Ieraksta Rediģēšana
-  </x-slot:title>
+    <x-slot:title>
+    Dienasgrāmatas ieraksta rediģēšana
+    </x-slot:title>
 
-  <h1>Ieraksta Rediģēšana</h1>
+    <div class="form-container">
+        <h1 class="page-title">Dienasgrāmatas ieraksta rediģēšana</h1>
 
-  <form method="POST" action="/diaries/{{ $diary->id }}">
-      @csrf
-      @method('PUT')
+        <form method="POST" action="/diaries/{{ $diary->id }}" class="blog-form">
+            @csrf
+            @method('PUT')
 
-      <div class="form-group">
-          <label for="title">Uzdevuma saturs:</label>
-          <input type="text" name="title" id="title" value="{{ $diary->title }}" required class="input-field"/>
-            @error("completed")
-            <p>{{ $message }}</p>
-            @enderror
-      </div>
+            <div class="form-group">
+                <label for="title">Tituls:</label>
+                <input type="text" name="title" id="title" value="{{ old('title', $diary->title) }}" required class="input-field"/>
+                @error('title')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
+            </div>
 
-      <div class="form-group">
-          <label for="body">Uzdevuma saturs:</label>
-          <input type="text" name="body" id="body" value="{{ $diary->body }}" required class="input-field"/>
-            @error("completed")
-            <p>{{ $message }}</p>
-            @enderror
-      </div>
+            <div class="form-group">
+                <label for="body">Saturs:</label>
+                <textarea name="body" id="body" required class="textarea-field">{{ old('body', $diary->body) }}</textarea>
+                @error('body')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
+            </div>
 
-      <div class="form-group">
-          <label for="date">Uzdevuma saturs:</label>
-          <input type="text" name="date" id="date" value="{{ $diary->date }}" required class="input-field"/>
-            @error("completed")
-            <p>{{ $message }}</p>
-            @enderror
-      </div>
+            <div class="form-group">
+                <label for="date">Datums:</label>
+                <input type="date" name="date" id="date" value="{{ old('date', $diary->date) }}" required class="input-field"/>
+                @error('date')
+                    <p class="error-message">{{ $message }}</p>
+                @enderror
+            </div>
 
-      <button type="submit" class="submit-button">Saglabāt izmaiņas</button>
-
-  </form>
+            <div class="button-container">
+                <button type="submit" class="submit-button">Saglabāt izmaiņas</button>
+            </div>
+        </form>
+    </div>
 </x-layout>
